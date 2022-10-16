8b792650e71b6e74a88e28398d0426f00b2e0f4a
ANASTASIIAZAIARNIUK <113503637+ANASTASIIAZAIARNIUK@users.noreply.github.com>
2022-10-16 15:39:51 +0200

    const canvasSketch = require('canvas-sketch'); const random = require('canvas-sketch-util/random');
    const settings = {
      dimensions: [ 2560, 1664 ],
      animate: true
      // was not sure how to make it windowWidth, windowHeight
    };
    
    const sketch = ({context, width, height}) => {
      const agents = []
    
      for (let i = 0; i < 220; i++){
        const x = random.range(0, width);
        const y = random.range(0, height);
        agents.push(new Agent(x,y));
      }
      return ({ context, width, height }) => {
        context.fillStyle = 'CYAN';
        context.fillRect(0, 0, width, height);
    
        for (let i = 0; i < agents.length; i++){
          const agent = agents[i];
    
          for (let j= i + 1; j < agents.length; j++ ) {
            const other = agents[j];
    
            const dist = agent.pos.getDistance(other.pos);
    
            if (dist > 200)continue;
    
            context.beginPath();
            context.moveTo(agent.pos.x, agent.pos.y);
            context.lineTo(other.pos.x, other.pos.y);
            context.stroke();
          }
        }
    
        agents.forEach(agent => {
          agent.update();
          agent.draw(context);
          agent.bounce (width, height);
        });
      };
    };
    
    canvasSketch(sketch, settings);
    
    class Vector {
      constructor(x, y){
        this.x = x;
        this.y = y;
      }
    
      getDistance(v) {
        const dx = this.x - v.x;
        const dy = this.y - v.y;
        return Math.sqrt(dx * dx + dy *dy);
      }
    }
    
    class Agent {
      constructor(x, y) {
        this.pos = new Vector(x, y);
        this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
        this.radius = random.range(10, 20);
      }
    
      bounce (width, height) {
        if (this.pos.x <= 0 || this.pos.x >=width) this.vel.x *= -1;
        if (this.pos.y <= 0 || this.pos.y >=height) this.vel.y *= -1;
      }
    
      update() {
        this.pos.x += this.vel.x;
        this.pos.y += this.vel.y;
      }
    
      draw(context){
    
        context.save();
        context.translate (this.pos.x, this.pos.y);
    
        context.lineWidth = 5;
    
        context.beginPath();
        context.arc(0, 0, this.radius, 0, Math.PI * 2);
        context.fill();
        context.stroke();
    
        context.restore();
      }
    }

--- sketch.js
+++ sketch.js
@@ -1,19 +1,3 @@
-//function preload() {
-	// put preload code here
-//}
-
-//function setup() {
-//createCanvas(windowWidth, windowHeight);
-// put setup code here
-//const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-//textAlign(CENTER, CENTER);
-//textSize(16)
-//text(message, width/2, height/2);
-
-
-//function draw() {
-// put drawing code here 
-
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
